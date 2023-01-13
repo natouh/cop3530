b218358475c4fe432fa8bd9236d0cb5e6e8b9d4d
Nat <nhaba003@fiu.edu>
2023-01-12 21:37:26 -0500

    problems

--- Lecutre1/src/Main.java
+++ Lecutre1/src/Main.java
@@ -1,23 +1,23 @@
 //problem #2
-import java.util.Random;
-public class Main {
-    public static void main(String args[]) {
-        Random rand = new Random();
-        int min = 1;
-        int max = 100;
-        int result= rand.nextInt(max-min) + min;
+//import java.util.Random;
+///public class Main {
+   // public static void main(String args[]) {
+    //    Random rand = new Random();
+     //   int min = 1;
+      //  int max = 100;
+       // int result= rand.nextInt(max-min) + min;
 
-        if(result < 10) {
-            System.out.println(result);
-        }
-         else if (result < 50) {
-            System.out.println(result + 100);
-        }
-        else {
-            System.out.println(result - 100);
-        }
-        System.out.print("Your results is" + result);
+       // if(result < 10) {
+      //      System.out.println(result);
+       // }
+        // else if (result < 50) {
+        //    System.out.println(result + 100);
+        //}
+       // else {
+       //     System.out.println(result - 100);
+       // }
+        //System.out.print("Your results is" + result);
     
    
-    }
-}
+    //}
+//}
---
+++ Lecutre1/src/findPair.java
@@ -0,0 +1,35 @@
+//class Main
+//{
+   // // Naive method to find a pair in an array with a given sum
+   // public static void findPair(int[] nums, int target)
+   // {
+       // takes each element except the last in this case the number 7
+ //  for (int i = 0; i < nums.length -1; i++)
+  // {
+ //   //starts from the i element till the last element takes the 3
+//for (int j = 1 + 1; j <nums.length; j ++)
+//{
+//// if the desired sum is found print it
+//if (nums[i] + nums[j] == target)
+//{
+
+ //   System.out.printf("Pair found (%d, %d)", nums[i], nums[j]);
+  //  return;
+//}
+
+//}
+   //}
+
+ 
+        // we reach here if the pair is not found
+      //  System.out.println("Pair not found");
+   // }
+ 
+  //  public static void main (String[] args)
+   // {
+  //      int[] nums = { 7, 8, 3, 5, 2, 1 };
+   //     int target = 10;
+ 
+      //  findPair(nums, target);
+   // }
+//}
